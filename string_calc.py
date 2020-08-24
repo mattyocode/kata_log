784104603858c93fd6592664fa849fb068a4c624
mattyocode <matthewoliver@live.co.uk>
2020-08-24 22:20:26 +0100

    First commit. Setting up calculator that takes string arguments with various delineators

---
+++ string_calc.py
@@ -0,0 +1,34 @@
+import re
+
+class StringCalc:
+
+    def split_string(self, str_num):
+        if str_num[:2] == '//':
+            spl_pnt = str_num[2]
+            lst = re.split(f"[{spl_pnt} \n]", str_num[3:])
+        else:
+            lst = re.split('[, \n]', str_num)
+        return lst
+
+    def to_int_list(self, str_num):
+        lst = self.split_string(str_num)
+        for i in range(len(lst)):
+            if lst[i] == "":
+                lst[i] = 0
+            else:
+                lst[i] = int(lst[i])
+        return lst
+
+    def check_negative(self, lst):
+        for item in lst:
+            if item < 0:
+                raise ValueError(f'Negatives not allowed. {item} entered as argument')
+                print(f'Negatives not allowed. {item} entered as argument')
+
+    def add_num(self, str_num):
+        lst = self.to_int_list(str_num)
+        self.check_negative(lst)
+        total = 0
+        for x in lst:
+            total += x
+        return total
---
+++ tests/test_string_calc.py
@@ -0,0 +1,33 @@
+from string_calc import *
+import pytest
+
+subject = StringCalc()
+
+def test_return_0_for_empty_string():
+    assert subject.add_num("") == 0
+
+def test_return_single_str_num_as_int():
+    assert subject.add_num("1") == 1
+
+def test_return_added_strs_as_int():
+    assert subject.add_num("1,2") == 3
+
+def test_3_args():
+    assert subject.add_num("1,2,3") == 6
+
+def test_5_args():
+    assert subject.add_num("3,3,3,3,3") == 15
+
+def test_input_seperated_by_linebreaks():
+    assert subject.add_num("1\n2,3") == 6
+
+def test_change_delimeter():
+    assert subject.add_num("//;\n1;2") == 3
+
+def test_change_delimeter_again():
+    assert subject.add_num("//!\n4!2") == 6
+
+def test_negatives_not_allowed():
+    with pytest.raises(ValueError) as e:
+        subject.add_num("1, -1")
+    assert e.type is ValueError
